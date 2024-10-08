216d9c40dbf18868de8a88be93ea8b77e3ea7b27
Chris St. Jean <chris.r.stjean@gmail.com>
2024-10-08 07:12:30 -0700

    testing pipeline

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,10 @@ fastify.get("/", function (request, reply) {
     reply.send({ hello: "world" });
 });
 
+fastify.get("/test", function (request, reply) {
+    reply.send("This works!");
+});
+
 // Run the server!
 // testing
 fastify.listen({ port: 8080, host: "0.0.0.0" }, function (err, address) {

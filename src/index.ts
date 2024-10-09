e83ee33ff4b72c8aea3de079a2592ef7ed4c4489
Chris St. Jean <chris.r.stjean@gmail.com>
2024-10-09 13:51:51 -0700

    testing sql connection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { config } from "dotenv";
 config();
 import Fastify, { FastifyReply, FastifyRequest } from "fastify";
+import getCloudSqlConnection from "./helpers/getCloudSqlConnection";
 
 const fastify = Fastify({
     logger: true,
@@ -8,6 +9,14 @@ const fastify = Fastify({
 
 fastify.get("/", function (request, reply) {
     reply.send({ hello: "world" });
+});
+
+fastify.get("/peaks", async function (request, reply) {
+    const connection = await getCloudSqlConnection();
+
+    const [rows] = await connection.execute("SELECT * FROM Peak");
+
+    return rows;
 });
 
 fastify.listen({ port: 8080, host: "0.0.0.0" }, function (err, address) {

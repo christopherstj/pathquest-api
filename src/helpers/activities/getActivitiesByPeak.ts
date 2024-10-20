09038e77941fa1b61af36572407149ced4ed20fb
Chris St. Jean <chris.r.stjean@gmail.com>
2024-10-20 14:17:06 -0700

    i guess i should push everything

---
+++ src/helpers/activities/getActivitiesByPeak.ts
@@ -0,0 +1,16 @@
+import { RowDataPacket } from "mysql2/promise";
+import getCloudSqlConnection from "../getCloudSqlConnection";
+import Activity from "../../typeDefs/Activity";
+
+const getActivityByPeak = async (peakId: string, userId: string) => {
+    const connection = await getCloudSqlConnection();
+
+    const [rows] = await connection.query<(Activity & RowDataPacket)[]>(
+        "SELECT a.* FROM ActivityPeak ap LEFT JOIN Activity a ON ap.activityId = a.id WHERE ap.peakId = ? AND a.userId = ?",
+        [peakId, userId]
+    );
+
+    return rows;
+};
+
+export default getActivityByPeak;
